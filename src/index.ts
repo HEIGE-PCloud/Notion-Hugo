2f0ebcebd592749b6c7185a61f50168c5ded5d61
HEIGE-PCloud <52968553+HEIGE-PCloud@users.noreply.github.com>
2022-08-22 17:29:09 +0100

    feat: render notion page into md files

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,40 @@
-import { Client } from "@notionhq/client";
+import { Client, isFullPage, iteratePaginatedAPI } from "@notionhq/client";
+import { PropertyItemListResponse, TitlePropertyItemObjectResponse } from "@notionhq/client/build/src/api-endpoints";
 import dotenv from "dotenv";
-import { exit } from "process";
-
+import { NotionToMarkdown } from "notion-to-md";
+import fs from "fs";
 dotenv.config();
+
 
 async function main() {
   const notion = new Client({
     auth: process.env.NOTION_TOKEN,
   });
 
+  const n2m = new NotionToMarkdown({ notionClient: notion });
+
+
   const DATABASE_ID = process.env.DATABASE_ID
   if (DATABASE_ID === undefined) {
-    console.error("DATABASE_ID has not been set")
-    exit(1)
+    throw "DATABASE_ID has not been set"    
   }
   
-  const response = await notion.databases.query({
-    database_id: DATABASE_ID,
-  });
 
-  console.log("Got response:", response);
+  for await (const page of iteratePaginatedAPI(notion.databases.query, {
+    database_id: DATABASE_ID
+  })) {
+    if (!isFullPage(page)) continue
+    const mdblocks = await n2m.pageToMarkdown(page.id);
+    const mdString = n2m.toMarkdownString(mdblocks);
+    const response = (await notion.pages.properties.retrieve({ page_id: page.id, property_id: 'title' })) as PropertyItemListResponse
+    const titleResponse = response.results[0] as TitlePropertyItemObjectResponse
+    const title = titleResponse.title.plain_text
+    const fileName = `${title}.md` 
+    console.log(mdString)
+    fs.writeFileSync(fileName, mdString);
+  
+  }
+  
 }
 
 main()
