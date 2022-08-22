fa782111bf81af366170ada008ff4f864d6566ea
HEIGE-PCloud <52968553+HEIGE-PCloud@users.noreply.github.com>
2022-08-22 16:57:22 +0100

    feat: set up database id

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { Client } from "@notionhq/client";
 import dotenv from "dotenv";
+import { exit } from "process";
 
 dotenv.config();
 
@@ -8,8 +9,14 @@ async function main() {
     auth: process.env.NOTION_TOKEN,
   });
 
+  const DATABASE_ID = process.env.DATABASE_ID
+  if (DATABASE_ID === undefined) {
+    console.error("DATABASE_ID has not been set")
+    exit(1)
+  }
+  
   const response = await notion.databases.query({
-    database_id: "FIXME",
+    database_id: DATABASE_ID,
   });
 
   console.log("Got response:", response);
