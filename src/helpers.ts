38466a096471d7541d5bb39945e667f23ec2a293
HEIGE-PCloud <52968553+HEIGE-PCloud@users.noreply.github.com>
2022-09-01 12:00:45 +0100

    refactor: allow mount pages

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,15 +1,23 @@
 import { exec } from "child_process";
-import toml from 'toml';
+import toml from "toml";
 import fs from "fs-extra";
 import { Client, isFullPage } from "@notionhq/client";
-import { PropertyItemListResponse, TitlePropertyItemObjectResponse } from "@notionhq/client/build/src/api-endpoints";
+import {
+  PageObjectResponse,
+  PropertyItemListResponse,
+  TitlePropertyItemObjectResponse,
+} from "@notionhq/client/build/src/api-endpoints";
+import { NotionToMarkdown } from "notion-to-md";
+import YAML from "yaml";
 
 /**
  * Execute simple shell command (async wrapper).
  * @param {String} cmd
  * @return {Object} { stdout: String, stderr: String }
  */
-export async function sh(cmd: string): Promise<{stdout: string, stderr: string}> {
+export async function sh(
+  cmd: string
+): Promise<{ stdout: string; stderr: string }> {
   return new Promise(function (resolve, reject) {
     exec(cmd, (err, stdout, stderr) => {
       if (err) {
@@ -21,38 +29,75 @@ export async function sh(cmd: string): Promise<{stdout: string, stderr: string}>
   });
 }
 
-export type mount = {
-  database_id: string,
-  target_folder: string,
-}
+export type pageMount = {
+  page_id: string;
+  target_folder: string;
+};
+
+export type databaseMount = {
+  database_id: string;
+  target_folder: string;
+};
 
 export type config = {
-  mounts: mount[]
-}
-
+  mount: {
+    databases: databaseMount[];
+    pages: pageMount[];
+  };
+};
 
 export function loadConfig(): config {
-  const configString = fs.readFileSync('config/notion.toml', 'utf8')      
-  const config = toml.parse(configString) as config
-  
-  if (config.mounts === undefined || config.mounts === null || config.mounts.length === 0) {
-    throw new SyntaxError("Error: No mounted folder is configured in notion.toml.");
+  const configString = fs.readFileSync("config/notion.toml", "utf8");
+  const config = toml.parse(configString) as config;
+
+  if (config.mount === undefined || config.mount === null) {
+    throw new SyntaxError("Error: No mount is configured in notion.toml.");
   }
 
-  return config
+  return config;
 }
 
-export async function getPageTitle(page_id: string, notion: Client): Promise<string> {
-  const response = (await notion.pages.properties.retrieve({ page_id, property_id: 'title' })) as PropertyItemListResponse
-  const titleResponse = response.results[0] as TitlePropertyItemObjectResponse
-  const title = titleResponse.title.plain_text
-  return title
+export async function getPageTitle(
+  page_id: string,
+  notion: Client
+): Promise<string> {
+  const response = (await notion.pages.properties.retrieve({
+    page_id,
+    property_id: "title",
+  })) as PropertyItemListResponse;
+  const titleResponse = response.results[0] as TitlePropertyItemObjectResponse;
+  const title = titleResponse.title.plain_text;
+  return title;
 }
 
-export async function getCoverLink(page_id: string, notion: Client): Promise<string | null> {
-  const page = await notion.pages.retrieve({ page_id })
-  if (!isFullPage(page)) return null
-  if (page.cover === null) return null
-  if (page.cover.type === 'external') return page.cover.external.url
-  else return page.cover.file.url
+export async function getCoverLink(
+  page_id: string,
+  notion: Client
+): Promise<string | null> {
+  const page = await notion.pages.retrieve({ page_id });
+  if (!isFullPage(page)) return null;
+  if (page.cover === null) return null;
+  if (page.cover.type === "external") return page.cover.external.url;
+  else return page.cover.file.url;
+}
+
+export async function renderPage(
+  page: PageObjectResponse,
+  notion: Client
+) {
+  const n2m = new NotionToMarkdown({ notionClient: notion });
+  const mdblocks = await n2m.pageToMarkdown(page.id);
+  const mdString = n2m.toMarkdownString(mdblocks);
+  const title = await getPageTitle(page.id, notion);
+  const featuredImageLink = await getCoverLink(page.id, notion);
+  const frontMatter = {
+    title,
+    date: page.created_time,
+    draft: false,
+    featuredImage: featuredImageLink ?? undefined,
+  };
+  return {
+    title,
+    pageString: '---\n' + YAML.stringify(frontMatter) + '\n---\n' + mdString
+  }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,56 +1,51 @@
 import { Client, isFullPage, iteratePaginatedAPI } from "@notionhq/client";
-import { PropertyItemListResponse, TitlePropertyItemObjectResponse } from "@notionhq/client/build/src/api-endpoints";
 import dotenv from "dotenv";
-import { NotionToMarkdown } from "notion-to-md";
 import fs from "fs-extra";
-import { getCoverLink, getPageTitle, loadConfig, sh } from "./helpers";
-import YAML from 'yaml'
+import { loadConfig, renderPage, sh } from "./helpers";
 
 dotenv.config();
 
-
 async function main() {
-  const config = loadConfig()
+  const config = loadConfig();
 
   const notion = new Client({
     auth: process.env.NOTION_TOKEN,
   });
 
-  const n2m = new NotionToMarkdown({ notionClient: notion });
-
-  for (const mount of config.mounts) {
+  // process mounted databases
+  for (const mount of config.mount.databases) {
+    fs.emptyDirSync(`content/${mount.target_folder}`);
     for await (const page of iteratePaginatedAPI(notion.databases.query, {
-      database_id: mount.database_id
+      database_id: mount.database_id,
     })) {
-      if (!isFullPage(page)) continue
-      const mdblocks = await n2m.pageToMarkdown(page.id);
-      const mdString = n2m.toMarkdownString(mdblocks);
-      const title = await getPageTitle(page.id, notion)
-      const featuredImageLink = await getCoverLink(page.id, notion);
-      const frontMatter = `---
-${YAML.stringify({
-  title,
-  date: page.created_time,
-  draft: false,
-  featuredImage: featuredImageLink ?? undefined  
-})}
----
-`
-      // clean the target folder
-      fs.emptyDirSync(`content/${mount.target_folder}`)
-      const fileName = title.replaceAll(' ', '-').toLowerCase()
-      let { stdout } = await sh(`hugo new ${mount.target_folder}/${fileName}.md`);
-      console.log(stdout)
-      fs.writeFileSync(`content/${mount.target_folder}/${fileName}.md`, frontMatter + mdString);
-    
+      if (!isFullPage(page)) continue;
+      const { title, pageString } = await renderPage(page, notion);
+      let { stdout } = await sh(
+        `hugo new "${mount.target_folder}/${title}.md"`
+      );
+      console.log(stdout);
+      fs.writeFileSync(
+        `content/${mount.target_folder}/${title}.md`,
+        pageString
+      );
     }
-      
   }
 
-  
+  // process mounted pages
+  for (const mount of config.mount.pages) {
+    const page = await notion.pages.retrieve({ page_id: mount.page_id });
+    if (!isFullPage(page)) continue
+    const { title, pageString } = await renderPage(page, notion);
+    let { stdout } = await sh(
+      `hugo new "${mount.target_folder}/${title}.md"`
+    );
+    console.log(stdout);
+    fs.writeFileSync(
+      `content/${mount.target_folder}/${title}.md`,
+      pageString
+    );
+  }
 }
-
-
 
 main()
   .then(() => process.exit(0))
