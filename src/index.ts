b33791d7fa372708e9cca2d503f8d96ecbdf4541
HEIGE-PCloud <heige.pcloud@outlook.com>
2023-08-10 13:05:56 +0100

    fix: build

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,8 @@ import dotenv from "dotenv";
 import fs from "fs-extra";
 import { savePage } from "./render";
 import { loadConfig } from "./config";
-import { getAllContentFiles, getContentFile } from "./file";
-import path from "path";
-import { getFileName, getPageTitle } from "./helpers";
+import { getAllContentFiles } from "./file";
+import { isFullPageOrDatabase } from "@notionhq/client/build/src/helpers";
 
 dotenv.config();
 
@@ -13,24 +12,26 @@ async function main() {
   if (process.env.NOTION_TOKEN === "")
     throw Error("The NOTION_TOKEN environment vairable is not set.");
   const config = await loadConfig();
-  console.info('[Info] Config loaded ')
+  console.info("[Info] Config loaded ");
 
   const notion = new Client({
     auth: process.env.NOTION_TOKEN,
   });
 
-  const page_ids: string[] = []
+  const page_ids: string[] = [];
 
-  console.info('[Info] Start processing mounted databases')
+  console.info("[Info] Start processing mounted databases");
   // process mounted databases
   for (const mount of config.mount.databases) {
     fs.ensureDirSync(`content/${mount.target_folder}`);
     for await (const page of iteratePaginatedAPI(notion.databases.query, {
       database_id: mount.database_id,
     })) {
-      if (!isFullPage(page)) continue;
-      console.info(`[Info] Start processing page ${page.id}`)
-      page_ids.push(page.id)
+      if (!isFullPageOrDatabase(page) || page.object !== "page") {
+        continue;
+      }
+      console.info(`[Info] Start processing page ${page.id}`);
+      page_ids.push(page.id);
       await savePage(page, notion, mount);
     }
   }
@@ -39,18 +40,17 @@ async function main() {
   for (const mount of config.mount.pages) {
     const page = await notion.pages.retrieve({ page_id: mount.page_id });
     if (!isFullPage(page)) continue;
-    page_ids.push(page.id)
+    page_ids.push(page.id);
     await savePage(page, notion, mount);
   }
 
   // remove posts that exist locally but not in Notion Database
-  const contentFiles = getAllContentFiles('content')
+  const contentFiles = getAllContentFiles("content");
   for (const file of contentFiles) {
     if (!page_ids.includes(file.metadata.id)) {
-      fs.removeSync(file.filepath)
+      fs.removeSync(file.filepath);
     }
   }
-  
 }
 
 main()
